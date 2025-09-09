c46f6d55be3f4d3aed09ca4e64b8cccf4659789a
Sumitasawa <sumitasawa65@gmail.com>
2025-09-09 17:41:55 +0530

    less

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -17,7 +17,7 @@ const Tour = ({ id, name, info, image, price, removeTour }) => {
             id={`see-more-${id}`}   
             onClick={() => setReadMore(!readMore)}
           >
-            {readMore ? "See Less" : "See more"}
+            {readMore ? "See less" : "See more"}
           </button>
         </p>
         <button
