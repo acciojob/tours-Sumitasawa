87e90c3c3dee8d11f827043d2fd1e11c54b47ea1
Sumitasawa <sumitasawa65@gmail.com>
2025-09-09 17:19:40 +0530

    First Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+const url = "https://cors-anywhere.herokuapp.com/https://course-api.com/react-tours-project/";
 
-const App = () => {
-    return(
-      <main id="main">
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+  const fetchTours = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setTours(tours);
+      setIsLoading(false);
+    } catch (error) {
+      console.error(error);
+      setIsLoading(false);
+    }
+  };
+  
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (isLoading) return <Loading />;
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <h2>No Tours Left</h2>
+        <button onClick={fetchTours}>Refresh</button>
       </main>
-    )
+    );
+  }
+
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
 }
+
 export default App;
